d1907fdf4c66739e2d1d6972311b908322e4f5a3
deepak rana <deepakrana@ieee.org>
2020-04-15 19:02:16 +0530

    login form added

--- Library Management System/Form1.Designer.cs
+++ Library Management System/Form1.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.loginAs = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(302, 75);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(145, 20);
+            this.Username.TabIndex = 0;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(302, 134);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(145, 20);
+            this.Password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "UserName";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(245, 225);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 23);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(372, 225);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 5;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Library_Management_System.Properties.Resources.unnamed;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(31, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 37);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "LOGIN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(211, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Login As";
+            // 
+            // loginAs
+            // 
+            this.loginAs.FormattingEnabled = true;
+            this.loginAs.Items.AddRange(new object[] {
+            "",
+            "Librarian ",
+            "Student "});
+            this.loginAs.Location = new System.Drawing.Point(302, 181);
+            this.loginAs.Name = "loginAs";
+            this.loginAs.Size = new System.Drawing.Size(145, 21);
+            this.loginAs.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(483, 285);
+            this.Controls.Add(this.loginAs);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Name = "Form1";
+            this.Text = "Login Window ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox loginAs;
     }
 }
 
--- Library Management System/Form1.cs
+++ Library Management System/Form1.cs
@@ -16,5 +16,20 @@ namespace Library_Management_System
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
