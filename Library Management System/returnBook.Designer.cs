3fb23860278db795c5e9e4108eb4439bf5de3bc6
deepak rana <deepakrana@ieee.org>
2020-04-20 12:59:06 +0530

    all Front end done from my end

---
+++ Library Management System/returnBook.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Library_Management_System
+{
+    partial class returnBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.submit = new System.Windows.Forms.Button();
+            this.bookDetails = new System.Windows.Forms.Label();
+            this.bookId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Clear.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(554, 392);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(99, 52);
+            this.Clear.TabIndex = 39;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.Red;
+            this.Exit.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.ForeColor = System.Drawing.SystemColors.Control;
+            this.Exit.Location = new System.Drawing.Point(312, 398);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(96, 46);
+            this.Exit.TabIndex = 38;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            // 
+            // submit
+            // 
+            this.submit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.submit.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(96, 392);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(105, 52);
+            this.submit.TabIndex = 37;
+            this.submit.Text = "Return Book";
+            this.submit.UseVisualStyleBackColor = false;
+            // 
+            // bookDetails
+            // 
+            this.bookDetails.AutoSize = true;
+            this.bookDetails.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.bookDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bookDetails.Location = new System.Drawing.Point(335, 175);
+            this.bookDetails.Name = "bookDetails";
+            this.bookDetails.Size = new System.Drawing.Size(99, 20);
+            this.bookDetails.TabIndex = 36;
+            this.bookDetails.Text = "Book Details";
+            // 
+            // bookId
+            // 
+            this.bookId.BackColor = System.Drawing.Color.LavenderBlush;
+            this.bookId.Location = new System.Drawing.Point(369, 104);
+            this.bookId.Name = "bookId";
+            this.bookId.Size = new System.Drawing.Size(177, 20);
+            this.bookId.TabIndex = 34;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(241, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Book Id";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightYellow;
+            this.label1.Location = new System.Drawing.Point(-37, 6);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(350, 9, 350, 9);
+            this.label1.Size = new System.Drawing.Size(894, 55);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Return Book";
+            // 
+            // returnBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Library_Management_System.Properties.Resources.library;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.bookDetails);
+            this.Controls.Add(this.bookId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "returnBook";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label bookDetails;
+        private System.Windows.Forms.TextBox bookId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
