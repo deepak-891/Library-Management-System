4b757848bb67a34fb30a622248bdfabf2c589264
deepak rana <deepakrana@ieee.org>
2020-04-21 12:28:50 +0530

    return pe ka kam chalu hai

--- Library Management System/returnBook.cs
+++ Library Management System/returnBook.cs
@@ -31,26 +31,63 @@ namespace Library_Management_System
 
         private void submit_Click(object sender, EventArgs e)
         {
-            string connectionString = "datasource=remotemysql.com;port=3306;username=4PWYWxK833;password=kcyuAYHJX7;database=4PWYWxK833;";                // using the code here... 
-            string query = "Select * from ISSUED where B_ID='"+ bookId.Text+"'";
-
-            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
-            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
-            {
-                CommandTimeout = 60
-            };
-
             try
             {
+               
+                string query = "", query2 = "";
+                string connectionString = "datasource=remotemysql.com;port=3306;username=4PWYWxK833;password=kcyuAYHJX7;database=4PWYWxK833;";                // using the code here... 
+                MySqlDataReader reader1, reader2;
+
+                query = "SELECT ISSUED.B_ID , ISSUED.S_ID, ISSUED.ISSUE_DATE, ISSUED.DUE_DATE, Book.B_NAME, Book.B_AUTH FROM " +
+                    "ISSUED,Book " +
+                    "WHERE Book.B_id = ISSUED.B_ID and ISSUED.B_ID='" + bookId.Text + "'";
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+                {
+                    CommandTimeout = 60
+                };
                 databaseConnection.Open();
-        
-            
-            
+                reader1 = commandDatabase.ExecuteReader();
+                String dueDate, student;
+                if (reader1.HasRows)
+                {
+                    while (reader1.Read())
+                    {
+
+                        student = reader1.GetString(1);
+                        dueDate = reader1.GetString(3);
+                        bookDetails.Text += "\nIssuing Student Id=" + reader1.GetString(1) +
+                            "\nIssue Date " + reader1.GetString(2) +
+                            "\nDue Date = " + reader1.GetString(3) +
+                            "\nBook name= " + reader1.GetString(4) +
+                            "\nBook Author= " + reader1.GetString(5);
+                         ;
+                    
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No Such Book exist");
+                }
+
+
+
+
+
+                MySqlConnection databaseConnection2 = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase2 = new MySqlCommand(query2, databaseConnection2)
+                {
+                    CommandTimeout = 60
+                };
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+
+
+
         }
     }
 
